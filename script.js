17b9a4204f4400f4454b24e64e9ceb45858ace21
Ceejayludick <ceejayludick13@gmail.com>
2023-06-14 12:50:41 +0200

    indentation
    aligning items correctly

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
 
 
-window.load=doShowAll();
+window.load = doShowAll();
 
 //Function below might be redundant.
 function CheckBrowser() {
-    if ('localStorage' in window && window['localStorage'] !== null) {
-        // We can use localStorage object to store data.
-        return true;
-    } else {
-            return false;
-    }
+  if ('localStorage' in window && window['localStorage'] !== null) {
+    // We can use localStorage object to store data.
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Dynamically populate the table with shopping list items.
 //Step below can be done via PHP and AJAX, too.
 function doShowAll() {
-    if (CheckBrowser()) {
-        var key = "";
-        var list = "<tr><th>Item</th><th>Value</th></tr>\n";
-        var i = 0;
-        //For a more advanced feature, you can set a cap on max items in the cart.
-        for (i = 0; i <= localStorage.length-1; i++) {
-            key = localStorage.key(i);
-            list += "<tr><td>" + key + "</td>\n<td>"
-                    + localStorage.getItem(key) + "</td></tr>\n";
-        }
-        //If no item exists in the cart.
-        if (list == "<tr><th>Item</th><th>Value</th></tr>\n") {
-            list += "<tr><td><i>empty</i></td>\n<td><i>empty</i></td></tr>\n";
-        }
-        //Bind the data to HTML table.
-        //You can use jQuery, too.
-        document.getElementById('list').innerHTML = list;
-    } else {
-        alert('Cannot save shopping list as your browser does not support HTML 5');
+  if (CheckBrowser()) {
+    var key = "";
+    var list = "<tr><th>Item</th><th>Value</th></tr>\n";
+    var i = 0;
+    //For a more advanced feature, you can set a cap on max items in the cart.
+    for (i = 0; i <= localStorage.length - 1; i++) {
+      key = localStorage.key(i);
+      list += "<tr><td>" + key + "</td>\n<td>"
+        + localStorage.getItem(key) + "</td></tr>\n";
     }
+    //If no item exists in the cart.
+    if (list == "<tr><th>Item</th><th>Value</th></tr>\n") {
+      list += "<tr><td><i>empty</i></td>\n<td><i>empty</i></td></tr>\n";
+    }
+    //Bind the data to HTML table.
+    //You can use jQuery, too.
+    document.getElementById('list').innerHTML = list;
+  } else {
+    alert('Cannot save shopping list as your browser does not support HTML 5');
+  }
 }
 
 
 function addProduct(target) {
-    // this is just a product placeholder
-    var productAdded = '<li class="cd-cart__product"><div class="cd-cart__image"><a href="#0"><img src="assets/img/product-preview.png" alt="placeholder"></a></div><div class="cd-cart__details"><h3 class="truncate"><a href="#0">Product Name</a></h3><span class="cd-cart__price">$25.99</span><div class="cd-cart__actions"><a href="#0" class="cd-cart__delete-item">Delete</a><div class="cd-cart__quantity"><label for="cd-product-'+ productId +'">Qty</label><span class="cd-cart__select"><select class="reset" id="cd-product-'+ productId +'" name="quantity"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option></select><svg class="icon" viewBox="0 0 12 12"><polyline fill="none" stroke="currentColor" points="2,4 6,8 10,4 "/></svg></span></div></div></div></li>';
-    cartList.insertAdjacentHTML('beforeend', productAdded);
-  };
+  // this is just a product placeholder
+  var productAdded = '<li class="cd-cart__product"><div class="cd-cart__image"><a href="#0"><img src="assets/img/product-preview.png" alt="placeholder"></a></div><div class="cd-cart__details"><h3 class="truncate"><a href="#0">Product Name</a></h3><span class="cd-cart__price">$25.99</span><div class="cd-cart__actions"><a href="#0" class="cd-cart__delete-item">Delete</a><div class="cd-cart__quantity"><label for="cd-product-' + productId + '">Qty</label><span class="cd-cart__select"><select class="reset" id="cd-product-' + productId + '" name="quantity"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option></select><svg class="icon" viewBox="0 0 12 12"><polyline fill="none" stroke="currentColor" points="2,4 6,8 10,4 "/></svg></span></div></div></div></li>';
+  cartList.insertAdjacentHTML('beforeend', productAdded);
+};
 
 /* Set rates + misc */
 
@@ -50,36 +50,35 @@ var fadeTime = 300;
 
 
 /* Assign actions */
-$('.product-quantity input').change( function() {
+$('.product-quantity input').change(function () {
   updateQuantity(this);
 });
 
-$('.product-removal button').click( function() {
+$('.product-removal button').click(function () {
   removeItem(this);
 });
 
 
 /* Recalculate cart */
-function recalculateCart()
-{
+function recalculateCart() {
   var subtotal = 0;
-  
+
   /* Sum up row totals */
   $('.product').each(function () {
     subtotal += parseFloat($(this).children('.product-line-price').text());
   });
-  
+
   /* Calculate totals */
   var tax = subtotal * taxRate;
   var shipping = (subtotal > 0 ? shippingRate : 0);
   var total = subtotal + tax + shipping;
-  
+
   /* Update totals display */
-  $('.totals-value').fadeOut(fadeTime, function() {
+  $('.totals-value').fadeOut(fadeTime, function () {
     $('#cart-total').html(total.toFixed(2));
-    if(total == 0){
+    if (total == 0) {
       $('.checkout').fadeOut(fadeTime);
-    }else{
+    } else {
       $('.checkout').fadeIn(fadeTime);
     }
     $('.totals-value').fadeIn(fadeTime);
@@ -88,31 +87,29 @@ function recalculateCart()
 
 
 /* Update quantity */
-function updateQuantity(quantityInput)
-{
+function updateQuantity(quantityInput) {
   /* Calculate line price */
   var productRow = $(quantityInput).parent().parent();
   var price = parseFloat(productRow.children('.product-price').text());
   var quantity = $(quantityInput).val();
   var linePrice = price * quantity;
-  
+
   /* Update line price display and recalc cart totals */
   productRow.children('.product-line-price').each(function () {
-    $(this).fadeOut(fadeTime, function() {
+    $(this).fadeOut(fadeTime, function () {
       $(this).text(linePrice.toFixed(2));
       recalculateCart();
       $(this).fadeIn(fadeTime);
     });
-  });  
+  });
 }
 
 
 /* Remove item from cart */
-function removeItem(removeButton)
-{
+function removeItem(removeButton) {
   /* Remove row from DOM and recalc cart total */
   var productRow = $(removeButton).parent().parent();
-  productRow.slideUp(fadeTime, function() {
+  productRow.slideUp(fadeTime, function () {
     productRow.remove();
     recalculateCart();
   });
