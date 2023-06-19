92d6ad8664c992360e0b37dd020d3ef0ea111f9f
Ceejayludick <ceejayludick13@gmail.com>
2023-06-19 17:23:57 +0200

    cart
    initial try of making the cart work by using JS and adding items to cart

--- README.md
+++ README.md
@@ -11,20 +11,20 @@ The project utilizes HTML for the structure of the web pages, CSS for styling an
 
 Code used and why:
 
-The three Coding languages used are HTML, CSS and Javascript. HTMl has been used to format the basic structure of the webpage and acocmapnying content that will be displayed on the Online Store. CSS was used for styling purposes; mainly with regard to colour choices, layout, sizing and fonts and backgrounds used for the app. Lastly, Javascript has been used mainly for interactivity purposes so that users inputs can dynamically input complex actions that HTML and CSS can't.
+The three Coding languages used are HTML, CSS and Javascript. HTMl has been used to format the basic structure of the webpage and accompanying content that will be displayed on the Online Store. CSS was used for styling purposes; mainly with regard to color choices, layout, sizing and fonts and backgrounds used for the app. Lastly, Javascript has been used mainly for interactivity purposes so that users inputs can dynamically input complex actions that HTML and CSS can't.
 
 Challenges faced:
 
 
 How to use Project:
 
-The project has the general layout of any webstore type website. The main headings used are basic parts to the webstore, and are: 
-1. An About section = wherein details about the company/store are listed, including a Mission and Vision, details about how the company/webstore was started and by who, as well as general Corporate information regarding the company.
+The project has the general layout of any web store type website. The main headings used are basic parts to the web store, and are: 
+1. An About section = wherein details about the company/store are listed, including a Mission and Vision, details about how the company/web store was started and by who, as well as general Corporate information regarding the company.
 2. A Shopping Cart section = wherein all the items selected to be 'shopped' will be displayed. Each of these items will also have an accompanying image and price tag banner.
-3. A Contact page = Wherein all details of the company is listed, including a Google Maps insert that shows the exact location of the companys physical address whilst also allowing the user to make easy use of Google Maps for direction purposes. Additonally, the companys Linkedin, Gmail, Github, Instagram and YouTube are also linked in the contact page. 
-4. A Lookbook page = showing all the differnet ranges of products available from the company.
+3. A Contact page = Wherein all details of the company is listed, including a Google Maps insert that shows the exact location of the company's physical address whilst also allowing the user to make easy use of Google Maps for direction purposes. Additionally, the company's Linkedin, Gmail, Github, Instagram and YouTube are also linked in the contact page. 
+4. A Look book page = showing all the different ranges of products available from the company.
 5. A Shop page = wherein the user will be able to make a selection from a range of products on display, which will also give the user the ability to add specific products to the shopping cart for purchase.
-6. A Home Page = which acts as the default page for the webstore. 
+6. A Home Page = which acts as the default page for the web store. 
 
 
 
--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 window.load = doShowAll();
 
 //Function below might be redundant.
@@ -12,112 +11,160 @@ function CheckBrowser() {
   }
 }
 
-// Dynamically populate the table with shopping list items.
-//Step below can be done via PHP and AJAX, too.
-function doShowAll() {
-  if (CheckBrowser()) {
-    var key = "";
-    var list = "<tr><th>Item</th><th>Value</th></tr>\n";
-    var i = 0;
-    //For a more advanced feature, you can set a cap on max items in the cart.
-    for (i = 0; i <= localStorage.length - 1; i++) {
-      key = localStorage.key(i);
-      list += "<tr><td>" + key + "</td>\n<td>"
-        + localStorage.getItem(key) + "</td></tr>\n";
-    }
-    //If no item exists in the cart.
-    if (list == "<tr><th>Item</th><th>Value</th></tr>\n") {
-      list += "<tr><td><i>empty</i></td>\n<td><i>empty</i></td></tr>\n";
-    }
-    //Bind the data to HTML table.
-    //You can use jQuery, too.
-    document.getElementById('list').innerHTML = list;
-  } else {
-    alert('Cannot save shopping list as your browser does not support HTML 5');
-  }
-}
-
-
 function addProduct(target) {
   // this is just a product placeholder
   var productAdded = '<li class="cd-cart__product"><div class="cd-cart__image"><a href="#0"><img src="assets/img/product-preview.png" alt="placeholder"></a></div><div class="cd-cart__details"><h3 class="truncate"><a href="#0">Product Name</a></h3><span class="cd-cart__price">$25.99</span><div class="cd-cart__actions"><a href="#0" class="cd-cart__delete-item">Delete</a><div class="cd-cart__quantity"><label for="cd-product-' + productId + '">Qty</label><span class="cd-cart__select"><select class="reset" id="cd-product-' + productId + '" name="quantity"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option></select><svg class="icon" viewBox="0 0 12 12"><polyline fill="none" stroke="currentColor" points="2,4 6,8 10,4 "/></svg></span></div></div></div></li>';
   cartList.insertAdjacentHTML('beforeend', productAdded);
 };
 
-/* Set rates + misc */
-
-var fadeTime = 300;
-
-
-/* Assign actions */
-$('.product-quantity input').change(function () {
-  updateQuantity(this);
-});
-
-$('.product-removal button').click(function () {
-  removeItem(this);
-});
-
-
-/* Recalculate cart */
-function recalculateCart() {
-  var subtotal = 0;
-
-  /* Sum up row totals */
-  $('.product').each(function () {
-    subtotal += parseFloat($(this).children('.product-line-price').text());
-  });
-
-  /* Calculate totals */
-  var tax = subtotal * taxRate;
-  var shipping = (subtotal > 0 ? shippingRate : 0);
-  var total = subtotal + tax + shipping;
-
-  /* Update totals display */
-  $('.totals-value').fadeOut(fadeTime, function () {
-    $('#cart-total').html(total.toFixed(2));
-    if (total == 0) {
-      $('.checkout').fadeOut(fadeTime);
-    } else {
-      $('.checkout').fadeIn(fadeTime);
-    }
-    $('.totals-value').fadeIn(fadeTime);
-  });
-}
-
-
-/* Update quantity */
-function updateQuantity(quantityInput) {
-  /* Calculate line price */
-  var productRow = $(quantityInput).parent().parent();
-  var price = parseFloat(productRow.children('.product-price').text());
-  var quantity = $(quantityInput).val();
-  var linePrice = price * quantity;
-
-  /* Update line price display and recalc cart totals */
-  productRow.children('.product-line-price').each(function () {
-    $(this).fadeOut(fadeTime, function () {
-      $(this).text(linePrice.toFixed(2));
-      recalculateCart();
-      $(this).fadeIn(fadeTime);
-    });
-  });
-}
-
-
-/* Remove item from cart */
-function removeItem(removeButton) {
-  /* Remove row from DOM and recalc cart total */
-  var productRow = $(removeButton).parent().parent();
-  productRow.slideUp(fadeTime, function () {
-    productRow.remove();
-    recalculateCart();
-  });
-}
-
-
 // linking products to DOM element 
 function myFunction() {
   const element = document.getElementById("myAnchor").href;
   document.getElementById("demo").innerHTML = element;
 }
+
+const btnCart=document.querySelector('#cart-icon');
+const cart=document.querySelector('.cart');
+const btnClose=document.querySelector('#cart-close');
+
+btnCart.addEventListener('click',()=>{
+  cart.classList.add('cart-active');
+});
+
+btnClose.addEventListener('click',()=>{
+  cart.classList.remove('cart-active');
+});
+
+document.addEventListener('DOMContentLoaded',loadFood);
+
+function loadFood(){
+  loadContent();
+
+}
+
+function loadContent(){
+  //Remove Food Items  From Cart
+  let btnRemove=document.querySelectorAll('.cart-remove');
+  btnRemove.forEach((btn)=>{
+    btn.addEventListener('click',removeItem);
+  });
+
+  //Product Item Change Event
+  let qtyElements=document.querySelectorAll('.cart-quantity');
+  qtyElements.forEach((input)=>{
+    input.addEventListener('change',changeQty);
+  });
+
+  //Product Cart
+  
+  let cartBtns=document.querySelectorAll('.add-cart');
+  cartBtns.forEach((btn)=>{
+    btn.addEventListener('click',addCart);
+  });
+
+  updateTotal();
+}
+
+
+//Remove Item
+function removeItem(){
+  if(confirm('Are Your Sure to Remove')){
+    let title=this.parentElement.querySelector('.cart-food-title').innerHTML;
+    itemList=itemList.filter(el=>el.title!=title);
+    this.parentElement.remove();
+    loadContent();
+  }
+}
+
+//Change Quantity
+function changeQty(){
+  if(isNaN(this.value) || this.value<1){
+    this.value=1;
+  }
+  loadContent();
+}
+
+let itemList=[];
+
+//Add Cart
+function addCart(){
+ let food=this.parentElement;
+ let title=food.querySelector('.food-title').innerHTML;
+ let price=food.querySelector('.food-price').innerHTML;
+ let imgSrc=food.querySelector('.food-img').src;
+ //console.log(title,price,imgSrc);
+ 
+ let newProduct={title,price,imgSrc}
+
+ //Check Product already Exist in Cart
+ if(itemList.find((el)=>el.title==newProduct.title)){
+  alert("Product Already added in Cart");
+  return;
+ }else{
+  itemList.push(newProduct);
+ }
+
+
+let newProductElement= createCartProduct(title,price,imgSrc);
+let element=document.createElement('div');
+element.innerHTML=newProductElement;
+let cartBasket=document.querySelector('.cart-content');
+cartBasket.append(element);
+loadContent();
+}
+
+
+function createCartProduct(title,price,imgSrc){
+
+  return `
+  <div class="cart-box">
+  <img src="${imgSrc}" class="cart-img">
+  <div class="detail-box">
+    <div class="cart-food-title">${title}</div>
+    <div class="price-box">
+      <div class="cart-price">${price}</div>
+       <div class="cart-amt">${price}</div>
+   </div>
+    <input type="number" value="1" class="cart-quantity">
+  </div>
+  <ion-icon name="trash" class="cart-remove"></ion-icon>
+</div>
+  `;
+}
+
+function updateTotal()
+{
+  const cartItems=document.querySelectorAll('.cart-box');
+  const totalValue=document.querySelector('.total-price');
+
+  let total=0;
+
+  cartItems.forEach(product=>{
+    let priceElement=product.querySelector('.cart-price');
+    let price=parseFloat(priceElement.innerHTML.replace("Rs.",""));
+    let qty=product.querySelector('.cart-quantity').value;
+    total+=(price*qty);
+    product.querySelector('.cart-amt').innerText="Rs."+(price*qty);
+
+  });
+
+  totalValue.innerHTML='Rs.'+total;
+
+
+  // Add Product Count in Cart Icon
+
+  const cartCount=document.querySelector('.cart-count');
+  let count=itemList.length;
+  cartCount.innerHTML=count;
+
+  if(count==0){
+    cartCount.style.display='none';
+  }else{
+    cartCount.style.display='block';
+  }
+
+
+}
+
+
+
